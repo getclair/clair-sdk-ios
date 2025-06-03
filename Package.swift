57f47f5286ced57b1b44a2c991f754e078fcc005
Nick Glaviano <nicholas@getclair.com>
2025-06-03 11:33:30 -0500

    fix: update package name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "clair-ios-sdk",
+    name: "Clair",
     platforms: [.iOS(.v16)],
     products: [
         .library(name: "Clair", targets: ["Clair"]),
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Clair",
-            url: "https://github.com/getclair/clair-sdk-ios/releases/download/v0.0.7/Clair.xcframework.zip",
+            url: "https://github.com/getclair/clair-sdk-ios/releases/download/v0.0.8/Clair.xcframework.zip",
             checksum: "ae35a7266048d2f3792eed1d050e46dfedbd702892e5a62549ae70e3a03df4f5"
         ),
     ]
