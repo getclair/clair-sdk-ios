37e6fce06aed8c5a3134b4c29b3ab858c0c24833
Rob Ambrose <rob@getclair.com>
2025-07-31 15:28:05 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Clair",
-            url: "https://github.com/getclair/clair-sdk-ios/releases/download/v0.0.21/Clair.xcframework.zip",
-            checksum: "961e05538d8e0432565139f5234ae4fb94e343d70bb4c6b99a242c1c2d093eed"
+            url: "https://github.com/getclair/clair-sdk-ios/releases/download/v0.0.22/Clair.xcframework.zip",
+            checksum: "78fe0b8d3c84e58113d871b8cbb8077ebca9e86d265d709643ac9d2e08c0678f"
         ),
     ]
 )
