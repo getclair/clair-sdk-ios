e132c6373b10b50fd37fa96ae6821d40b592e8b5
Nick Glaviano <nicholas@getclair.com>
2025-08-28 13:54:15 -0500

    feat: release v0.0.23

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Clair",
-            url: "https://github.com/getclair/clair-sdk-ios/releases/download/v0.0.22/Clair.xcframework.zip",
-            checksum: "78fe0b8d3c84e58113d871b8cbb8077ebca9e86d265d709643ac9d2e08c0678f"
+            url: "https://github.com/getclair/clair-sdk-ios/releases/download/v0.0.23/Clair.xcframework.zip",
+            checksum: "15bd2741a01076e9254f5f7cc7aec77034fbc30c4aee7a3076ff92f24d851a0d"
         ),
     ]
 )
